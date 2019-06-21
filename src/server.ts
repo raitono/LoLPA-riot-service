6489bf2e68d1fdd22f4d8c20f4ec77efdba47ba0
Raitono <bpaintx2@gmail.com>
2019-06-20 20:00:55 -0400

    Add service bits from template

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,26 @@
 import app from './app';
+import { AddressInfo } from 'net';
+import axios from 'axios';
 const debug: any = require('debug')('summoner-service:server');
 
-// Process.env will always be comprised of strings, so we typecast the port to a number.
-const PORT:number = Number(process.env.PORT) || 3000;
+const server = app.listen(0);
+const port = (<AddressInfo>server.address()).port;
 
-export default app.listen(PORT, () => debug('Service listening...'));
+const register = () => {
+  debug('registering...');
+
+  // tslint:disable-next-line: max-line-length
+  axios.put(`${process.env.REGISTRY_URL}:${process.env.REGISTRY_PORT}/service/register/${process.env.SERVICE_NAME}/${process.env.SERVICE_VERSION}/${port}`)
+    .then((res) => {
+      debug(res.data);
+      debug('registed!');
+    });
+};
+
+register();
+
+const interval = setInterval(register, Number(process.env.REGISTER_INTERVAL) * 1000);
+
+debug(`Service listening on port ${port}...`);
+
+export default server;
