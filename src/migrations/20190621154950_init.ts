f34046e60b656f6a8ad595844aae09e77ec38680
Raitono <bpaintx2@gmail.com>
2019-06-23 12:46:15 -0400

    Change to natural key

--- src/migrations/20190621154950_init.ts
+++ src/migrations/20190621154950_init.ts
@@ -4,8 +4,7 @@ import Knex = require('knex');
 exports.up = async function (knex: Knex) {
   return knex.schema
     .createTable('summoners', (table) => {
-      table.increments('id');
-      table.string('puuid', 100);
+      table.string('puuid', 100).primary();
       table.string('summonerId', 63);
       table.string('accountId', 56);
       table.string('name', 50);
