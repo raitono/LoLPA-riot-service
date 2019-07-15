966ada5317c8d63083ae586ac6ca51a9934aa539
raitono <bpaintx2@gmail.com>
2019-07-15 14:28:22 -0400

    Refacto Champion to use fromAPI method

--- src/models/champion.model.ts
+++ src/models/champion.model.ts
@@ -46,18 +46,67 @@ export class Champion extends Model {
   static get idColumn() {
     return 'id';
   }
-  static relationMappings = {
-    tags: {
-      relation: Model.ManyToManyRelation,
-      modelClass: Tag,
-      join: {
-        from: 'champions.id',
-        through: {
-          from: 'championTags.championId',
-          to: 'championTags.tagId',
+  static get relationMappings() {
+    return {
+      tags: {
+        relation: Model.ManyToManyRelation,
+        modelClass: Tag,
+        join: {
+          from: 'champions.id',
+          through: {
+            from: 'championTags.championId',
+            to: 'championTags.tagId',
+          },
+          to: 'tags.id',
         },
-        to: 'tags.id',
       },
-    },
-  };
+    };
+  }
+  static async fromAPI(champs: Object) {
+    const tags = await Tag.query();
+    return Promise.all(Object.keys(champs).map(async (id) => {
+      const apiChampion: Object = champs[id];
+      const dbChampion = new Champion();
+
+      dbChampion.id = apiChampion.id;
+      dbChampion.name = apiChampion.name;
+      dbChampion.title = apiChampion.title;
+      dbChampion.blurb = apiChampion.blurb;
+      dbChampion.attack = apiChampion.info.attack;
+      dbChampion.defense = apiChampion.info.defense;
+      dbChampion.magic = apiChampion.info.magic;
+      dbChampion.difficulty = apiChampion.info.difficulty;
+      dbChampion.imageFull = apiChampion.image.full;
+      dbChampion.imageSprite = apiChampion.image.sprite;
+      dbChampion.imageGroup = apiChampion.image.group;
+      dbChampion.imageX = apiChampion.image.x;
+      dbChampion.imageY = apiChampion.image.y;
+      dbChampion.imageW = apiChampion.image.w;
+      dbChampion.imageH = apiChampion.image.h;
+      dbChampion.partype = apiChampion.partype;
+      dbChampion.hp = apiChampion.stats.hp;
+      dbChampion.hpperlevel = apiChampion.stats.hpperlevel;
+      dbChampion.mp = apiChampion.stats.mp;
+      dbChampion.mpperlevel = apiChampion.stats.mpperlevel;
+      dbChampion.movespeed = apiChampion.stats.movespeed;
+      dbChampion.armor = apiChampion.stats.armor;
+      dbChampion.armorperlevel = apiChampion.stats.armorperlevel;
+      dbChampion.spellblock = apiChampion.stats.spellblock;
+      dbChampion.spellblockperlevel = apiChampion.stats.spellblockperlevel;
+      dbChampion.attackrange = apiChampion.stats.attackrange;
+      dbChampion.hpregen = apiChampion.stats.hpregen;
+      dbChampion.hpregenperlevel = apiChampion.stats.hpregenperlevel;
+      dbChampion.mpregen = apiChampion.stats.mpregen;
+      dbChampion.mpregenperlevel = apiChampion.stats.mpregenperlevel;
+      dbChampion.crit = apiChampion.stats.crit;
+      dbChampion.critperlevel = apiChampion.stats.critperlevel;
+      dbChampion.attackdamage = apiChampion.stats.attackdamage;
+      dbChampion.attackdamageperlevel = apiChampion.stats.attackdamageperlevel;
+      dbChampion.attackspeedperlevel = apiChampion.stats.attackspeedperlevel;
+      dbChampion.attackspeed = apiChampion.stats.attackspeed;
+      dbChampion.tags = tags.filter(t => apiChampion.tags.includes(t.name));
+
+      return dbChampion;
+    }));
+  }
 }
--- src/services/static/champion.service.ts
+++ src/services/static/champion.service.ts
@@ -13,8 +13,6 @@ export class ChampionService {
   static async patchChampions() {
     const c: {[key:string]: Object} = (await this.getChampions()).data;
     const apiTags: Set<string> = new Set();
-    const tags: Tag[] = [];
-    const champions: Champion[] = [];
 
     // Grab all the tags from the API
     // Use a Set to prevent duplicates
@@ -27,56 +25,11 @@ export class ChampionService {
 
     await Tag.upsertGraphFromList(apiTags);
 
-    const dbTags: Tag[] = await Tag.query();
-
-    Object.keys(c).forEach((championId) => {
-      const apiChampion = c[championId];
-      const dbChampion = new Champion();
-
-      dbChampion.id = apiChampion.id;
-      dbChampion.name = apiChampion.name;
-      dbChampion.title = apiChampion.title;
-      dbChampion.blurb = apiChampion.blurb;
-      dbChampion.attack = apiChampion.info.attack;
-      dbChampion.defense = apiChampion.info.defense;
-      dbChampion.magic = apiChampion.info.magic;
-      dbChampion.difficulty = apiChampion.info.difficulty;
-      dbChampion.imageFull = apiChampion.image.full;
-      dbChampion.imageSprite = apiChampion.image.sprite;
-      dbChampion.imageGroup = apiChampion.image.group;
-      dbChampion.imageX = apiChampion.image.x;
-      dbChampion.imageY = apiChampion.image.y;
-      dbChampion.imageW = apiChampion.image.w;
-      dbChampion.imageH = apiChampion.image.h;
-      dbChampion.partype = apiChampion.partype;
-      dbChampion.hp = apiChampion.stats.hp;
-      dbChampion.hpperlevel = apiChampion.stats.hpperlevel;
-      dbChampion.mp = apiChampion.stats.mp;
-      dbChampion.mpperlevel = apiChampion.stats.mpperlevel;
-      dbChampion.movespeed = apiChampion.stats.movespeed;
-      dbChampion.armor = apiChampion.stats.armor;
-      dbChampion.armorperlevel = apiChampion.stats.armorperlevel;
-      dbChampion.spellblock = apiChampion.stats.spellblock;
-      dbChampion.spellblockperlevel = apiChampion.stats.spellblockperlevel;
-      dbChampion.attackrange = apiChampion.stats.attackrange;
-      dbChampion.hpregen = apiChampion.stats.hpregen;
-      dbChampion.hpregenperlevel = apiChampion.stats.hpregenperlevel;
-      dbChampion.mpregen = apiChampion.stats.mpregen;
-      dbChampion.mpregenperlevel = apiChampion.stats.mpregenperlevel;
-      dbChampion.crit = apiChampion.stats.crit;
-      dbChampion.critperlevel = apiChampion.stats.critperlevel;
-      dbChampion.attackdamage = apiChampion.stats.attackdamage;
-      dbChampion.attackdamageperlevel = apiChampion.stats.attackdamageperlevel;
-      dbChampion.attackspeedperlevel = apiChampion.stats.attackspeedperlevel;
-      dbChampion.attackspeed = apiChampion.stats.attackspeed;
-      dbChampion.tags = dbTags.filter(t => apiChampion.tags.includes(t.name));
-
-      champions.push(dbChampion);
-    });
+    const champions = await Champion.fromAPI(c);
 
     await Champion.query().upsertGraph(
       champions,
-      { relate: true, unrelate: true },
+      { relate: true, unrelate: true, insertMissing: true },
     );
   }
 }
